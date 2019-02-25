d04628c6b5d114bca91dd98dee7bc4513e492593
KOH,BRIAN <brian.koh@keysight.com>
2019-02-25 17:15:07 +0800

    Migrate to Angular 7

--- src/test.ts
+++ src/test.ts
@@ -2,17 +2,19 @@
 
 import 'zone.js/dist/zone-testing';
 import { getTestBed } from '@angular/core/testing';
-import { BrowserDynamicTestingModule, platformBrowserDynamicTesting } from '@angular/platform-browser-dynamic/testing';
+import {
+  BrowserDynamicTestingModule,
+  platformBrowserDynamicTesting
+} from '@angular/platform-browser-dynamic/testing';
 
 declare const require: any;
 
 // First, initialize the Angular testing environment.
-getTestBed().initTestEnvironment(BrowserDynamicTestingModule, platformBrowserDynamicTesting());
+getTestBed().initTestEnvironment(
+  BrowserDynamicTestingModule,
+  platformBrowserDynamicTesting()
+);
 // Then we find all the tests.
-// const context = require.context('./', true, /\.spec\.ts$/);
-// // And load the modules.
-// context.keys().map(context);
-
-// const context1 = require.context('../lib/src/pixie-highcharts/test', true, /\.spec\.ts$/);
-const context1 = require.context('../lib/src', true, /\.spec\.ts$/);
-context1.keys().map(context1);
+const context = require.context('./', true, /\.spec\.ts$/);
+// And load the modules.
+context.keys().map(context);
